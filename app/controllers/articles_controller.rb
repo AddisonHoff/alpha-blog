110df5e17504dbcf823ce6e93567f8bd5c9b08c0
Addison Hoff <addisonghoff@gmail.com>
2018-06-09 21:24:07 +0000

    Add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+    
+    def index
+       @articles = Article.all 
+        
+    end
+    
     def new
         @article = Article.new
         
@@ -8,7 +14,7 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created"
-        redirect_to article_path(@article)
+            redirect_to article_path(@article)
         
         else 
         render :new
